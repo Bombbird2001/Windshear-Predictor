baf4853cbe3b6fc984141d0fa89435fed2db05e6
Shang Yi Ku <kushangyi2001@gmail.com>
2022-03-07 15:38:03 +0800

    Added initial models - 1 linear, 1 neural

--- parse_to_csv.py
+++ parse_to_csv.py
@@ -37,6 +37,22 @@ def matchWs(metar):
 	return "0" if re.search(r" WS ", metar) is None else "1"
 
 
+def matchRain(metar):
+	result = re.search(r" (-|\+)*[A-Z]*(RA) ", metar)
+	if result is None:
+		return ("0", "0")
+	grp1 = result.group(1)
+	grp2 = result.group(2)
+	if grp2 != "RA":
+		return ("0", "0")
+	if grp1 is None:
+		return ("0.6", "1")
+	elif grp1 == "-":
+		return ("0.3", "1")
+	elif grp1 == "+":
+		return ("1", "1")
+
+
 test_ratio = 0.2
 files = sys.argv
 files.pop(0)
@@ -46,8 +62,8 @@ for file in files:
 		data = f.read().split("\n")
 		with open(file + "/" + file + "_train.csv", "w+") as output:
 			with open(file + "/" + file + "_test.csv", "w+") as test:
-				output.write("windSpdKts,gustKts,windVrbDeg,visibilityMtrs,qnh,ws")
-				test.write("windSpdKts,gustKts,windVrbDeg,visibilityMtrs,qnh,ws")
+				output.write("windSpdKts,gustKts,windVrbDeg,intensity,rain,visibilityMtrs,qnh,ws")
+				test.write("windSpdKts,gustKts,windVrbDeg,intensity,rain,visibilityMtrs,qnh,ws")
 				for line in data:
 					filterData = " ".join(line.split("BECMG")[0].split("TEMPO")[0].split("PROB30")[0].split("PROB40")[0].split("RMK")[0].split(" ")[4:])
 					try:
@@ -57,7 +73,8 @@ for file in files:
 							continue
 						if windGust is None:
 							windGust = "0"
-						text = "\n" + windSpd + "," + windGust + "," + matchWindVrb(filterData) + "," + matchVisibility(filterData) + "," + matchQnh(filterData) + "," + matchWs(filterData)
+						intensity, rain = matchRain(filterData)
+						text = "\n" + windSpd + "," + windGust + "," + matchWindVrb(filterData) + "," + intensity + "," + rain + "," + matchVisibility(filterData) + "," + matchQnh(filterData) + "," + matchWs(filterData)
 						test.write(text) if random.random() <= test_ratio else output.write(text)
 					except Exception as e:
 						print(e)
